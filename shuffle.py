fa2de28e6d24b829b08657512f8d112ab52261ab
ZhangLei <RyanCheung98@163.com>
2021-11-27 12:58:35 +0800

    :zap: shuffle algorithm for more reasonable

--- shuffle.py
+++ shuffle.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from math import ceil
+from math import ceil, floor
 from typing import Tuple, List
 
 import numpy as np
@@ -14,7 +14,7 @@ class Shuffle(object):
     def __init__(
             self,
             split_indice: Tuple[float, float, float] = (0.8, 0.1, 0.7),
-            task_indice: Tuple[int, int] = (5, 10),
+            task_indice: Tuple[int, int] = (5, 8),
     ):
         """
         Shuffle the Datapack images to fcl tasks
@@ -40,25 +40,28 @@ class Shuffle(object):
             # if the number of camera datapack is more than that of edge node,
             # then merge the last 2 camera datapack into 1 camera datapack.
             if len(datapack.pack) > camera_num:
-                # sorted_pack = sorted(
-                #     [(cam, len(person)) for cam, person in datapack.pack.items()],
-                #     key=lambda t: t[1]
-                # )
-                # first_last_camera_id = sorted_pack[-1][0]
-                # second_last_camera_id = sorted_pack[-2][0]
-                first_last_camera_id = list(datapack.pack.keys())[-1]
-                second_last_camera_id = list(datapack.pack.keys())[-2]
+                sorted_pack = sorted(
+                    [(cam, len(person)) for cam, person in datapack.pack.items()],
+                    key=lambda t: t[1]
+                )
+                min_cam_id = sorted_pack[0][0]
+                min_cam_persons = datapack.pack[min_cam_id].keys()
+                most_diff_cam_id = sorted_pack[1][0]
+                most_diff_cam_persons = datapack.pack[most_diff_cam_id].keys()
+                for cam_id, _ in sorted_pack:
+                    curr_cam_persons = datapack.pack[cam_id].keys()
+                    if len(min_cam_persons - curr_cam_persons) > len(min_cam_persons - most_diff_cam_persons):
+                        most_diff_cam_id = cam_id
+                        most_diff_cam_persons = curr_cam_persons
 
-                # copy all person ids from last camera datapack to
-                # the second last camera pack.
-                for person_id, img_list in datapack.pack[first_last_camera_id].items():
-                    if person_id not in datapack.pack[second_last_camera_id].keys():
-                        datapack.pack[second_last_camera_id][person_id] = []
-                    datapack.pack[second_last_camera_id][person_id].extend(img_list)
+                # merge two camera id
+                for person_id, img_list in datapack.pack[min_cam_id].items():
+                    if person_id not in datapack.pack[most_diff_cam_id].keys():
+                        datapack.pack[most_diff_cam_id][person_id] = []
+                    datapack.pack[most_diff_cam_id][person_id].extend(img_list)
 
-                # remove the last camera datapack.
-                del datapack.pack[first_last_camera_id]
-                datapack.current_camera -= 1
+                # remove one camera datapack.
+                del datapack.pack[min_cam_id]
 
             # if the number of camera datapack is less than that of edge node,
             # then split the biggest camera datapack into 2 camera datapack.
@@ -81,6 +84,13 @@ class Shuffle(object):
                 for person_id, img_list in datapack.pack[new_cam_id].items():
                     del datapack.pack[max_cam_id][person_id]
 
+        # rebase the camera id
+        _datapack = {}
+        for idx, (camera_id, person_seq) in enumerate(datapack.pack.items(), 0):
+            _datapack[idx] = dict(sorted(person_seq.items(), key=lambda ite: ite[0]))
+        datapack.pack = dict(sorted(_datapack.items(), key=lambda ite: ite[0]))
+        datapack.current_camera = len(_datapack.keys()) - 1
+
     @staticmethod
     def save_imgs(img_path_list: List, save_dir: str):
         if not os.path.exists(save_dir):
@@ -99,10 +109,16 @@ class Shuffle(object):
             batch_id = -1
             batch_size = ceil(len(person_seq) / self.task_indice[1])
 
+            empty_query = False
             # divide equally by person_id with each camera
             for idx, (person_id, img_list) in enumerate(person_seq.items()):
-                if idx % batch_size == 0:
+
+                if idx % batch_size == 0 and batch_id + 1 < self.task_indice[1]:
+                    if empty_query:
+                        print("empty query for: camera {}, batch {}.".format(cam_id, batch_id))
+                    empty_query = True
                     batch_id += 1
+
                 task_save_dir = os.path.join(output, f'task-{cam_id}-{batch_id}')
                 tr_save_dir = os.path.join(task_save_dir, 'train', f'{person_id}')
                 query_save_dir = os.path.join(task_save_dir, 'query', f'{person_id}')
@@ -112,25 +128,58 @@ class Shuffle(object):
                 np.random.seed(seed)
                 np.random.shuffle(img_list)
 
-                # train, query and gallery image list has following roles:
+                # img_list is the total images from current person id and camera.
+                # and the train, query and gallery list has following roles:
                 # for example the split indice is (0.8, 0.1, 0.7)
-                # train  : random sample 80% from image list;
-                # query  : random sample 10% from image list that
-                #          is differ from train datasets;
-                # gallery: contain the rest of image list that never been used
-                #          by train and query dataset, and the 70% image in
-                #          train dataset.
-                tr_split_pivot = self.split_indice[0] * img_list_size
-                query_split_pivot = (self.split_indice[0] + self.split_indice[1]) * img_list_size
+                # train  : random sample 80% from img_list;
+                # query  : random sample 10% from other cameras that is differ
+                #          from gallery and train cameras; if other camera not
+                #          exists, then random choice 10% from train img_list.
+                # gallery: contain the rest of img_list that has never been used
+                #          by train and query dataset, and the 70% image in train
+                #          dataset.
 
-                tr_img_list = img_list[:int(tr_split_pivot)]
-                query_img_list = img_list[int(tr_split_pivot): int(query_split_pivot)]
-                gallery_img_list = np.random.choice(
+                tr_img_list = []
+                query_img_list = []
+                gallery_img_list = []
+                split_pivot = self.split_indice[0] * img_list_size
+
+                # choose the fist images before split pivot as train images
+                tr_img_list.extend(img_list[:floor(split_pivot)])
+
+                # choose query images from different cameras
+                for other_cam_id, other_person_seq in datapack.pack.items():
+                    if cam_id == other_cam_id:
+                        continue
+                    if person_id in other_person_seq.keys():
+                        np.random.seed(seed)
+                        query_img_list.extend(np.random.choice(
+                            other_person_seq[person_id],
+                            size=int(len(other_person_seq[person_id]) * self.split_indice[1]),
+                            replace=False
+                        ).tolist())
+
+                # if other camera not exists, then choose the part of images from current cameras
+                # after split pivot, and push the split pivot forward.
+                # if len(query_img_list) == 0:
+                #     pre_split_pivot = split_pivot
+                #     split_pivot = (self.split_indice[0] + self.split_indice[1]) * img_list_size
+                #     query_img_list.extend(img_list[floor(pre_split_pivot):ceil(split_pivot)])
+                pre_split_pivot = split_pivot
+                split_pivot = (self.split_indice[0] + self.split_indice[1]) * img_list_size
+                query_img_list.extend(img_list[floor(pre_split_pivot):ceil(split_pivot)])
+
+                # choose rest of current camera images and some part of training images as gallery
+                gallery_img_list.extend(np.random.choice(
                     tr_img_list,
-                    size=int(img_list_size * self.split_indice[2]),
+                    size=ceil(len(tr_img_list) * self.split_indice[2]),
                     replace=False
-                ).tolist() + img_list[int(query_split_pivot):]
+                ).tolist() + img_list[ceil(split_pivot):])
 
-                self.save_imgs(tr_img_list, tr_save_dir)
-                self.save_imgs(query_img_list, query_save_dir)
-                self.save_imgs(gallery_img_list, gallery_save_dir)
+                if len(tr_img_list):
+                    self.save_imgs(tr_img_list, tr_save_dir)
+                if len(query_img_list):
+                    empty_query = False
+                    self.save_imgs(query_img_list, query_save_dir)
+                if len(gallery_img_list):
+                    self.save_imgs(gallery_img_list, gallery_save_dir)
